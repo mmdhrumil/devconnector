c5b72b86a4c9e9c4d3dd14fad8269c6206957aaf
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-01 14:12:31 -0500

    Seventh commit: User login and Authentication token

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,8 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 // @route POST api/users
@@ -60,7 +60,23 @@ async (req, res) => {
 
         await user.save();
 
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 360000 },
+            (err, token) => {
+                if(err){
+                    throw err;
+                }
+                res.json({ token });
+            });
+
 
 
     }catch(err){
