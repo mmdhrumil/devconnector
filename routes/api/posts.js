c126bb4327db82515edd903c4adc48c4630035ee
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-08 13:56:59 -0500

    Seventeenth Commit: Add user post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const {check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Post'); 
+const Post = require('../../models/Post');
 
-// @route GET api/posts
-// @description Test route for adding users
-// @access Public
 
-router.get('/', (req, res) => res.send('Posts route'));
+// @route POST api/posts
+// @description Create a post
+// @access Private
+
+router.post('/', [
+    auth,
+    [
+        check('text', 'Text is required').not().isEmpty(),
+    ]
+],
+async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    try{
+        const user = await User.findById(req.user.id);
+        
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+
+    }catch(err){
+        console.log(err.message);
+        return res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
