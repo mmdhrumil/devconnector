f11c2ef099d907cd625592443c880c6825ce3b40
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-09 18:26:32 -0500

    Twentieth commit: Add and Delete a comment on a user's post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -181,4 +181,85 @@ async (req, res) => {
     }
 });
 
+
+// @route POST api/posts/comment/:id
+// @description Comment on a post
+// @access Private
+
+router.post('/comment/:id', [
+    auth,
+    [
+        check('text', 'Text is required').not().isEmpty(),
+    ]
+],
+async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    try{
+        const user = await (await User.findById(req.user.id)).isSelected('-password');
+        const post = await Post.findById(req.params.id);
+
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+        res.json(post.comments);
+
+    }catch(err){
+        console.log(err.message);
+        return res.status(500).send("Server Error");
+    }
+});
+
+
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @description Delete a comment on a post
+// @access Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+
+        // Pull out the comment to be deleted
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure comment exists
+        if(!comment){
+            return res.status(404).json({msg: "Comment not found for the ID."});
+        }
+        
+        // Check if the user is authorized to delete
+        if(comment.user.toString() !== req.user.id){
+            return res.status(401).json({msg: "User not authorized to delete the comment"});
+        }
+        else{
+            // Get remove index 
+            const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+            post.comments.splice(removeIndex, 1);
+
+            await post.save();
+            return res.json(post.comments);
+        }
+
+    }catch(err){
+        console.log(err.message);
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg: "Post not found for the ID."});
+        }
+        res.status(500).send("Server error");
+    }
+});
+
 module.exports = router;
