e04865c5845f892bfcdd47dd7fb95d53108661b9
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-19 15:46:57 -0500

    25: Added login form for the users

--- client/src/Components/auth/Login.js
+++ client/src/Components/auth/Login.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const onChange = e => setFormData({
+        ...formData, [e.target.name]: e.target.value
+    });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+
+    const {email, password} = formData;
+
     return(
-        <div>
-            <p>Login</p>
-        </div>
+        <Fragment>
+            <section className="container">
+                <h1 className="large text-primary">Sign In</h1>
+                <p className="lead"><i className="fas fa-user"></i> Sign into your account.</p>
+                <form className="form" onSubmit={e => onSubmit(e)}>
+                    
+                    <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" value = {email} onChange={e => onChange(e)} required/>
+                    </div>
+                    <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value = {password}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                    />
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="Login" />
+                </form>
+                <p className="my-1">
+                    Don't have an account? <Link to="/register">Sign Up</Link>
+                </p>
+            </section>
+        </Fragment>
     );
 }
+
 
 export default Login;
--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Register = () => {
 
@@ -14,7 +15,7 @@ const Register = () => {
         ...formData, [e.target.name]: e.target.value
     });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
             console.log('Passwords do not match');
@@ -24,7 +25,7 @@ const Register = () => {
         }
     }
 
-    
+
     const {name, email, password, password2} = formData;
 
     return(
@@ -66,7 +67,7 @@ const Register = () => {
                     <input type="submit" className="btn btn-primary" value="Register" />
                 </form>
                 <p className="my-1">
-                    Already have an account? <a href="login.html">Sign In</a>
+                    Already have an account? <Link to="/login">Sign In</Link>
                 </p>
             </section>
         </Fragment>
