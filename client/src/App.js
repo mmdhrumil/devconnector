cc68057c96e27f1551fb2a96e705918d332c1a35
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-19 14:43:53 -0500

    23: Added Routes, Register form and Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/Landing';
-
+import Login from './Components/auth/Login';
+import Register from './Components/auth/Register';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component = {Landing} />
+
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component = {Register}/>
+            <Route exact path="/login" component = {Login}/>
+          </Switch>
+
+        </section>
+
+      </Fragment>
+    </Router>
   );
 }
 
