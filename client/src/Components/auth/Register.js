5ad0b7596c42e1ab9e2bd3c88a1b6f4ab3309450
mdhrumil <dhrumil.m@somaiya.edu>
2020-01-21 14:46:29 -0500

    28: Added authentication reducer and action.

--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -3,8 +3,9 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setAlert} from '../../actions/alert'
 import PropTypes from 'prop-types';
+import {register} from '../../actions/auth';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -13,6 +14,9 @@ const Register = ({ setAlert }) => {
         password2: '',
 
     });
+
+    const {name, email, password, password2} = formData;
+
 
     const onChange = e => setFormData({
         ...formData, [e.target.name]: e.target.value
@@ -24,12 +28,9 @@ const Register = ({ setAlert }) => {
             setAlert('Passwords do not match', 'danger');
         }
         else{
-            console.log('SUCCESS');
+            register({name, email, password});
         }
     }
-
-
-    const {name, email, password, password2} = formData;
 
     return(
         <Fragment>
@@ -38,10 +39,10 @@ const Register = ({ setAlert }) => {
                 <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
                 <form className="form" onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" value = {name} onChange={e => onChange(e)} required />
+                    <input type="text" placeholder="Name" name="name" value = {name} onChange={e => onChange(e)}  />
                     </div>
                     <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value = {email} onChange={e => onChange(e)} required/>
+                    <input type="email" placeholder="Email Address" name="email" value = {email} onChange={e => onChange(e)} />
                     <small className="form-text"
                         >This site uses Gravatar so if you want a profile image, use a
                         Gravatar email
@@ -78,7 +79,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register})(Register);
